1f8976d1e90b5bab51bb8d96cc8c35c333248e89
vltanh <narubees@gmail.com>
2021-09-01 23:35:38 +0700

    implement GM-CPHD, minor changes for GM-PHD

--- rfs/src/filters/__init__.py
+++ rfs/src/filters/__init__.py
@@ -1,1 +1,2 @@
 from .phd import *
+from .cphd import *
--- rfs/src/filters/phd/gms.py
+++ rfs/src/filters/phd/gms.py
@@ -23,10 +23,11 @@ class PHD_GMS_Filter:
     def run(self, Z):
         K = len(Z)
 
-        # w_preds, m_preds, P_preds = [[]], [[]], [[]]
         w_upds = [np.array([1.])]
         m_upds = [np.zeros((1, self.model.x_dim))]
         P_upds = [np.eye(self.model.x_dim)[np.newaxis, :]]
+
+        w_ests, m_ests, P_ests = [[]], [[]], [[]]
 
         for k in range(1, K + 1):
             # == Predict ==
@@ -38,20 +39,15 @@ class PHD_GMS_Filter:
             P_preds_k = np.empty((N+L, self.model.x_dim, self.model.x_dim))
 
             # Predict surviving states
-            w_preds_k[:N] = self.model.P_S * w_upds[-1]
-            m_preds_k[:N], P_preds_k[:N] = \
+            w_preds_k[L:] = self.model.P_S * w_upds[-1]
+            m_preds_k[L:], P_preds_k[L:] = \
                 kalman_predict(self.model.F, self.model.Q,
                                m_upds[-1], P_upds[-1])
 
             # Predict born states
-            w_preds_k[N:] = self.model.w_birth
-            m_preds_k[N:] = self.model.m_birth
-            P_preds_k[N:] = self.model.P_birth
-
-            # Log (optional)
-            # m_preds.append(m_preds_k)
-            # P_preds.append(P_preds_k)
-            # w_preds.append(w_preds_k)
+            w_preds_k[:L] = self.model.w_birth
+            m_preds_k[:L] = self.model.m_birth
+            P_preds_k[:L] = self.model.P_birth
 
             # == Gating ==
             cand_Z = Z[k-1]
@@ -65,29 +61,29 @@ class PHD_GMS_Filter:
             N2 = cand_Z.shape[0]
             M = N1 * (N2 + 1)
 
-            # Miss detection
             m_upds_k = np.empty((M, self.model.x_dim))
             P_upds_k = np.empty((M, self.model.x_dim, self.model.x_dim))
             w_upds_k = np.empty((M,))
 
+            # Miss detection
             m_upds_k[:N1] = m_preds_k.copy()
             P_upds_k[:N1] = P_preds_k.copy()
             w_upds_k[:N1] = (1 - self.model.P_D) * w_preds_k
 
             # Detection
-            if len(cand_Z) > 0:
+            if N2 > 0:
                 qs, ms, Ps = kalman_update(cand_Z,
                                            self.model.H, self.model.R,
                                            m_preds_k, P_preds_k)
-
-                P_upds_k[N1:] = np.tile(Ps, (N2, 1, 1))
-                m_upds_k[N1:] = ms.transpose(
-                    1, 0, 2).reshape(-1, self.model.x_dim)
 
                 w = self.model.P_D * w_preds_k * qs.T
                 w = w / (self.model.lambda_c * self.model.pdf_c +
                          w.sum(1)[:, np.newaxis])
                 w_upds_k[N1:] = w.reshape(-1)
+
+                m_upds_k[N1:] = \
+                    ms.transpose(1, 0, 2).reshape(-1, self.model.x_dim)
+                P_upds_k[N1:] = np.tile(Ps, (N2, 1, 1))
 
             # == Post-processing ==
             w_upds_k, m_upds_k, P_upds_k = prune(
@@ -99,8 +95,18 @@ class PHD_GMS_Filter:
                 self.merge_threshold, self.L_max)
 
             # Log
+            w_upds.append(w_upds_k)
             m_upds.append(m_upds_k)
             P_upds.append(P_upds_k)
-            w_upds.append(w_upds_k)
 
-        return w_upds, m_upds, P_upds
+            # == Estimate ==
+            cnt = w_upds_k.round().astype(np.int32)
+            w_ests_k = w_upds_k.repeat(cnt, axis=0)
+            m_ests_k = m_upds_k.repeat(cnt, axis=0)
+            P_ests_k = P_upds_k.repeat(cnt, axis=0)
+
+            w_ests.append(w_ests_k)
+            m_ests.append(m_ests_k)
+            P_ests.append(P_ests_k)
+
+        return w_ests, m_ests, P_ests
